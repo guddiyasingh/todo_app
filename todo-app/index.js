3a7d5fd35606784064d650d85a62ae3257193e98
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-07-28 20:24:32 +0530

    add some code in file index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -7,4 +7,10 @@ app.use(express.json()); // To parse Json bodies
 
 //connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/todo-app',)
-.then(()=>console.log())
+.then(()=>console.log("MongoDb connected"))
+.catch(err => console.log(err));
+
+// Start server
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000")
+});
